2bbbb5bc54715b9461d4b6707bfbcc9a6c994f6d
Jun <i2r.jun@gmail.com>
2016-09-16 16:29:22 +0900

    Example code

---
+++ example/init.lua
@@ -0,0 +1,56 @@
+metemq = require "metemq"
+
+WIFI_NAME = "WIFI_SSID"
+PASSWORD = "WIFI_PASSWORD"
+BROKER_HOST = "example.com"
+BROKER_PORT = "1883"
+THING_ID = "MY_NODEMCU"
+
+function connectThing()
+  -- Create Thing
+  thing = metemq.Thing(THING_ID)
+
+  -- If you want to see MQTT messages, remove comment below
+  -- thing:setupLogger()
+
+  -- Connect to MeteMQ broker
+  thing:connect(BROKER_HOST, {
+      port = BROKER_PORT,
+      onConnect = function()
+        sub = thing:subscribe("demo")
+
+        sub:on({
+            added = function(name, age)
+              print("added", name, age)
+            end,
+            changed = function(name, age)
+              print(name, "->", age)
+            end,
+            removed = function(id)
+              print("removed id")
+            end
+          })
+
+        thing:call("hello", function(err, result)
+            if(err) then return print("there was error") end
+            print("hello:", result)
+          end)
+
+        local temp = thing:bind("temp")
+        temp:set(math.random(1000000))
+      end
+    })
+end
+
+wifi.setmode(wifi.STATION)
+wifi.sta.config(WIFI_NAME, PASSWORD)
+wifi.sta.connect()
+tmr.alarm(1, 1000, 1, function()
+    if wifi.sta.getip() == nil then
+      print("Connecting...")
+    else
+      tmr.stop(1)
+      print("Connected, IP is "..wifi.sta.getip())
+      connectThing()
+    end
+  end)
